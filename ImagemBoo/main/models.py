cd4f19b6504088f7a4366e33eeebd2bfb76b99d0
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-21 15:40:15 +0300

    create base models - Category and Image

--- ImagemBoo/main/models.py
+++ ImagemBoo/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=64, verbose_name="Image Category")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Image Category"
+        verbose_name_plural = "Image Category"
+        ordering = ["created_at"] # Sort by create date
+
+
+class Image(models.Model):
+
+    file = models.ImageField(upload_to="uploads/%Y/%m/%d")
+    description = models.TextField(blank=True, verbose_name='Description')
+    delete = models.BooleanField(default=False)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, to_field='id',on_delete=models.CASCADE, related_name='image_category', verbose_name="Image Category" )
+    user = models.ForeignKey(get_user_model(),on_delete=models.SET_NULL, related_name='image_user', null = True, default=None, verbose_name="Image Uploader") 
+
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Image"
+        ordering = ["uploaded_at"] # Sort by Upload date
+
