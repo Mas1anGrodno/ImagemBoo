25fb00d40d4ad0593cd34a037686b25f20c17b9e
Kryhin Maksim <masian.grodno@gmail.com>
2024-09-03 13:55:00 +0300

    add test images + making base functionality to side menu. Category selection working fine

--- ImagemBoo/ImagemBoo/settings.py
+++ ImagemBoo/ImagemBoo/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/' # add media_url to image storage
+MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
--- ImagemBoo/main/views.py
+++ ImagemBoo/main/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from main.models import *
 
-def home(request):
-    images = Image.objects.all()
+class ImagesListAll(ListView):
+    model = Image
+    template_name = 'main/image_view.html'
 
-    context = {
-        'title': 'All pictures',
-        'images': images,
-        'header': "All pictures"
-        }
-    return render(request, 'main/base.html', context)
+    def get_queryset(self):
+        return Image.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'ImagemBoo - All Images'
+        return context
+    
+class ImagesList(ListView):
+    model = Image
+    template_name = 'main/image_view.html'
+
+    def get_queryset(self):
+        category_name = self.kwargs['category_name']
+        category = get_object_or_404(Category, name=category_name)
+        return Image.objects.filter(category=category)
