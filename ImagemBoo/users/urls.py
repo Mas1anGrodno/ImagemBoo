456cd0ea839f8c3dc9985001a76f38e6b9cee20d
Kryhin Maksim <masian.grodno@gmail.com>
2024-09-03 17:31:22 +0300

    add user creation form

--- ImagemBoo/users/urls.py
+++ ImagemBoo/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path("login/", LoginUser.as_view(), name='login'),
     path("logout/", logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
 ]
--- ImagemBoo/users/views.py
+++ ImagemBoo/users/views.py
@@ -3,7 +3,9 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import LoginView
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
+from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
@@ -14,3 +16,9 @@ class LoginUser(LoginView):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Регистрация"}
+    success_url = reverse_lazy('users:login')
