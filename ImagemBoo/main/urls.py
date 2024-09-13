376bf8910dd57ba59ec953f8a208fc53c9302cc7
Kryhin Maxim <kryhin.amzn@gmail.com>
2024-09-13 11:31:26 +0300

    add image view page + css for image_selected_view

--- ImagemBoo/main/urls.py
+++ ImagemBoo/main/urls.py
@@ -3,31 +3,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import *
-#------------------------------- API ------------------------------------
+
+# ------------------------------- API ------------------------------------
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'images', ImageViewSet)
+router.register(r"images", ImageViewSet)
 
 urlpatterns = [
-    path("", ImagesListAll.as_view(),name='home'),
-    path("<str:category_name>/", ImagesListByCategory.as_view(), name='images_by_category'),
-    path('main/image_add/', AddPicture.as_view(), name='image_add'),
-    path('main/image_user/', UserPictures.as_view(), name='images_user'),
-    
-    path('main/user/<str:user_name>/', UserInspect.as_view(), name='inspect_user'),
-    path('main/user/<str:user_name>/delete/', UserDelete.as_view(), name='delete_user'),
-    path('user/<str:username>/images/', SelectedUserImages.as_view(), name='selected_user_images'),
-    
-    path('main/image_deleted/', DeletedPictures.as_view(), name='images_deleted'),
-    path('main/image_edit/<int:pk>/', EditPicture.as_view(), name='image_edit'),
-    path('main/delete/<int:pk>/', DeletePicture.as_view(), name='image_delete'),
-    path('main/restore/<int:pk>/', RestorePicture.as_view(), name='image_restore'),
-    
-#------------------------------- API ------------------------------------
-    path('api/v1/', include(router.urls)),
-    
-    
+    path("", ImagesListAll.as_view(), name="home"),
+    path("<str:category_name>/", ImagesListByCategory.as_view(), name="images_by_category"),
+    path("main/image_add/", AddPicture.as_view(), name="image_add"),
+    path("main/image_user/", UserPictures.as_view(), name="images_user"),
+    path("image/<int:pk>/", ImageSelectedView.as_view(), name="image_selected_view"),
+    path("main/user/<str:user_name>/", UserInspect.as_view(), name="inspect_user"),
+    path("main/user/<str:user_name>/delete/", UserDelete.as_view(), name="delete_user"),
+    path("user/<str:username>/images/", SelectedUserImages.as_view(), name="selected_user_images"),
+    path("main/image_deleted/", DeletedPictures.as_view(), name="images_deleted"),
+    path("main/image_edit/<int:pk>/", EditPicture.as_view(), name="image_edit"),
+    path("main/delete/<int:pk>/", DeletePicture.as_view(), name="image_delete"),
+    path("main/restore/<int:pk>/", RestorePicture.as_view(), name="image_restore"),
+    # ------------------------------- API ------------------------------------
+    path("api/v1/", include(router.urls)),
 ]
 
 if settings.DEBUG:
--- ImagemBoo/main/views.py
+++ ImagemBoo/main/views.py
@@ -1,121 +1,139 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from main.models import *
 from .forms import *
 
-#------------------------------- API ------------------------------------
+# ------------------------------- API ------------------------------------
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .serializers import ImageSerializer
 
+
 class ImagesListAll(ListView):
     model = Image
-    template_name = 'main/image_view.html'
+    template_name = "main/image_view.html"
 
     def get_queryset(self):
         return Image.objects.filter(delete=False)
-    
+
+
 class ImagesListByCategory(ListView):
     model = Image
-    template_name = 'main/image_view.html'
+    template_name = "main/image_view.html"
 
     def get_queryset(self):
-        category_name = self.kwargs['category_name']
+        category_name = self.kwargs["category_name"]
         category_selected = get_object_or_404(Category, name=category_name)
         return Image.objects.filter(category=category_selected, delete=False)
-    
+
+
 class AddPicture(CreateView):
     model = Image
-    template_name = 'main/image_add.html'
-    fields = ['file', 'description', 'category']
-    success_url = reverse_lazy('images_user')
-    
+    template_name = "main/image_add.html"
+    fields = ["file", "description", "category"]
+    success_url = reverse_lazy("images_user")
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+
 class EditPicture(UpdateView):
     model = Image
-    template_name = 'main/image_edit.html'
-    fields = ['description', 'category']
-    success_url = reverse_lazy('home')
+    template_name = "main/image_edit.html"
+    fields = ["description", "category"]
+    success_url = reverse_lazy("home")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['image'] = self.object
-        context['form'] = self.get_form()
+        context["image"] = self.object
+        context["form"] = self.get_form()
         return context
+
+
+class ImageSelectedView(DetailView):
+    model = Image
+    template_name = "main/image_selected_view.html"
+    context_object_name = "image"
+
 
 class DeletePicture(UpdateView):
     model = Image
-    fields = ['delete']
-    template_name = 'main/image_confirm.html'
-    success_url = reverse_lazy('images_user') 
+    fields = ["delete"]
+    template_name = "main/image_confirm.html"
+    success_url = reverse_lazy("images_user")
 
     def form_valid(self, form):
         form.instance.delete = True
         return super().form_valid(form)
 
+
 class RestorePicture(UpdateView):
     model = Image
-    fields = ['delete']
-    template_name = 'main/image_confirm.html'
-    success_url = reverse_lazy('images_user') 
+    fields = ["delete"]
+    template_name = "main/image_confirm.html"
+    success_url = reverse_lazy("images_user")
 
     def form_valid(self, form):
         form.instance.delete = False
         return super().form_valid(form)
-    
+
+
 class UserPictures(LoginRequiredMixin, ListView):
     model = Image
-    template_name = 'main/image_user.html'
+    template_name = "main/image_user.html"
 
     def get_queryset(self):
         return Image.objects.filter(user=self.request.user, delete=False)
+
+
 class SelectedUserImages(LoginRequiredMixin, ListView):
     model = Image
-    template_name = 'main/image_selected_user.html'
+    template_name = "main/image_selected_user.html"
 
     def get_queryset(self):
-        user = User.objects.get(username=self.kwargs['username'])
+        user = User.objects.get(username=self.kwargs["username"])
         return Image.objects.filter(user=user, delete=False)
-    
+
+
 class DeletedPictures(LoginRequiredMixin, ListView):
     model = Image
-    template_name = 'main/image_deleted.html'
+    template_name = "main/image_deleted.html"
 
     def get_queryset(self):
         return Image.objects.filter(user=self.request.user, delete=True)
 
+
 class UserInspect(UpdateView):
     model = User
     form_class = UserInspectForm
-    template_name = 'main/user_inspect.html'
-    success_url = reverse_lazy('images_user')
+    template_name = "main/user_inspect.html"
+    success_url = reverse_lazy("images_user")
 
     def get_object(self, queryset=None):
-        return User.objects.get(username=self.kwargs['user_name'])
+        return User.objects.get(username=self.kwargs["user_name"])
 
     def form_valid(self, form):
         user = form.save(commit=False)
-        password = form.cleaned_data.get('password')
+        password = form.cleaned_data.get("password")
         if password:
             user.set_password(password)
         user.save()
         return super().form_valid(form)
-    
+
+
 class UserDelete(DeleteView):
     model = User
-    template_name = 'main/user_confirm_delete.html'
-    success_url = reverse_lazy('home')
+    template_name = "main/user_confirm_delete.html"
+    success_url = reverse_lazy("home")
 
     def get_object(self, queryset=None):
-        return User.objects.get(username=self.kwargs['user_name'])
-    
+        return User.objects.get(username=self.kwargs["user_name"])
 
-#------------------------------- API ------------------------------------
+
+# ------------------------------- API ------------------------------------
 
 
 class ImageViewSet(viewsets.ModelViewSet):
@@ -125,4 +143,3 @@ class ImageViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
